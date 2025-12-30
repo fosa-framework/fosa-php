1e84e6f241f8e39fa9c294ca878dfc4dc78760d2
Mendrika Rabeh <frabehevitra@gmail.com>
2025-12-31 00:31:40 +0100

    chore: remove AuthController and ExampleController, update routes in index.php

--- index.php
+++ index.php
@@ -4,18 +4,21 @@ require __DIR__ . '/autoload.php';
 
 use Fosa\Core\Router;
 use Fosa\Controllers\AppController;
-use Fosa\Controllers\ExampleController;
-use Fosa\Controllers\AuthController;
+// Uncomment the following line to add more controller examples
+// use Fosa\Controllers\ExampleController;
 
 $router = new Router();
 
 /* API routes */
-$router->route('/api/say-hello', 'GET', ExampleController::class);
-$router->route('/api/auth/authenticate', 'POST', AuthController::class, 'login');
+$router->route('/api/say-hello', 'GET', AppController::class, 'hello');
+// Uncomment the following line to add more API route examples
+// $router->route('/api/example/show', 'GET', ExampleController::class, 'show');
 
 
 /* WEB routes */
 $router->route('/', 'GET', AppController::class);
+// Uncomment the following line to add more WEB route examples
+// $router->route('/example', 'GET', ExampleController::class);
 
 $router->run();
 
